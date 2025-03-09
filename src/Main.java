0671d3679346a84f2a232bcd80fff7f7a6c08abf
bntik <bannikov.ilya2017@yandex.ru>
2025-03-09 11:55:42 +0500

    homework_ArraysPartTwo

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,29 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.Collections;
+
 
 public class Main {
     public static void main(String[] args) {
         //задание №1
-        int[] payment={123,256,112,999,456};
+        int[] payment = {123, 256, 112, 999, 456};
         int sum = 0;
-        for (int element : payment){
+        for (int element : payment) {
             sum += element;
         }
         System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+        //задание №2
+        int min = Arrays.stream(payment).min().getAsInt();
+        int max = Arrays.stream(payment).max().getAsInt();
+        System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей. Макимальная сумма трат за неделю составила " + max + " рублей");
+        //задание №3
+        double averagePayment;
+        averagePayment = sum / payment.length;//чтобы не создавать лишние сущности, использую сумму трат из первого задания
+        System.out.println("Средняя сумма трат за месяц составила " + averagePayment + " рублей");
+        //задание №4
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+         for (int i = reverseFullName.length-1; i >= 0 ; i--){
+             System.out.print(reverseFullName[i]);
+
+         }
     }
 }
 
