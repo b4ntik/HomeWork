63b57404ad1ba343518e8865bbc5ed529f184a10
bntik <bannikov.ilya2017@yandex.ru>
2025-03-09 16:34:57 +0500

    homework_arrays_refactor

--- src/Main.java
+++ src/Main.java
@@ -11,19 +11,31 @@ public class Main {
         }
         System.out.println("Сумма трат за месяц составила " + sum + " рублей");
         //задание №2
-        int min = Arrays.stream(payment).min().getAsInt();
-        int max = Arrays.stream(payment).max().getAsInt();
+        int min = payment[0];
+        int max = payment[0];
+        //цикл для поиска минимального и максимального значений в массиве
+        for (int i = 0; i < payment.length; i++) {
+            if (payment[i] < min) {
+                min = payment[i];
+            } else if (payment[i] > max) {
+                max = payment[i];
+            }
+        }
+
         System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей. Макимальная сумма трат за неделю составила " + max + " рублей");
         //задание №3
         double averagePayment;
         averagePayment = sum / payment.length;//чтобы не создавать лишние сущности, использую сумму трат из первого задания
         System.out.println("Средняя сумма трат за месяц составила " + averagePayment + " рублей");
         //задание №4
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-         for (int i = reverseFullName.length-1; i >= 0 ; i--){
-             System.out.print(reverseFullName[i]);
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 0; i < reverseFullName.length / 2; i++) {
+            char temp = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length - 1 - i];
+            reverseFullName[reverseFullName.length - 1 - i] = temp;
 
-         }
+        }
+        System.out.print(reverseFullName);
     }
 }
 
