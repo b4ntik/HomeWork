8e1be6f1c20380ab86a5889a2c2de0d378d57409
bntik <bannikov.ilya2017@yandex.ru>
2025-03-09 12:41:11 +0500

    homework_String

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,10 @@ public class Main {
         //задание №2
         String fullNameUpper = fullName.toUpperCase();
         System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameUpper);
+        //задание №3
+        fullName = "Иванов Семён Семёнович";
+        String newFullName = fullName.replace ('ё', 'е');
+        System.out.println(newFullName);
          }
     }
 
