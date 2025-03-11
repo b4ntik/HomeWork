1e8b7001445f215da0b300270bf18794d363c7b6
bntik <bannikov.ilya2017@yandex.ru>
2025-03-11 20:12:26 +0500

    initial commit

---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book{
+    public String bookName;
+    public String author;
+    public int publishYear;
+    public Book (String bookName, String author, int publishYear){
+        this.bookName = bookName;
+        this.author = author;
+        this.publishYear = publishYear;
+    }
+}
+public class Author {
+    public String authorName;
+    public String authorFamily;
+    public Author (String authorName, String authorFamily){
+        this.authorName = authorName;
+        this.authorFamily = authorFamily;
+    }
+}
+//publi
+public static void main(String[] args) {
+}
+
+
+
+
+
+
